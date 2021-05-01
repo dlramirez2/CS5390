51e128b1b1876837acc79f612e49dec0cf225e78
luclarke <52232599+luclarke@users.noreply.github.com>
2021-04-30 19:45:36 -0600

    standalone scripts to run Open Attack models

---
+++ oa_demo/word_demo.py
@@ -0,0 +1,56 @@
+import OpenAttack
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import numpy as np
+from tqdm import tqdm
+
+def make_model():
+    class MyClassifier(OpenAttack.Classifier):
+        def __init__(self):
+            try:
+                self.model = SentimentIntensityAnalyzer()
+            except LookupError:
+                nltk.download('vader_lexicon')
+                self.model = SentimentIntensityAnalyzer()
+            
+        def get_prob(self, input_):
+            ret = []
+            for sent in input_:
+                res = self.model.polarity_scores(sent)
+                prob = (res["pos"] + 1e-6) / (res["neg"] + res["pos"] + 1e-6)
+                ret.append(np.array([1 - prob, prob]))
+            return np.array(ret)
+    return MyClassifier()
+
+
+def main():
+
+    print("Word-Level Attacks")
+    attacker = OpenAttack.attackers.PWWSAttacker()
+    #attacker = OpenAttack.attackers.HotFlipAttacker()
+
+    print("Build model")
+    clsf = make_model()
+    
+    dataset = OpenAttack.DataManager.loadDataset("SST.sample")[15:20]
+
+    print("Start attack")
+    options = {
+        "success_rate": True,
+        "fluency": False,
+        "mistake": False,
+        "semantic": False,
+        "levenstein": True,
+        "word_distance": False,
+        "modification_rate": True,
+        "running_time": True,
+
+        "invoke_limit": 500,
+        "average_invoke": True
+    }
+    attack_eval = OpenAttack.attack_evals.InvokeLimitedAttackEval(attacker, clsf, **options )
+    attack_eval.eval(dataset, visualize=True)
+    
+    
+if __name__ == "__main__":
+    main()
