9a29ec9ba86f80aee9983079605ead0e98f1b0cc
Diana Ramirez <dlramirez2@miners.utep.edu>
2022-04-18 16:18:34 -0600

    Add files via upload
    Code refactoring updates

--- oa_demo/word_demo.py
+++ oa_demo/word_demo.py
@@ -1,5 +1,6 @@
 import OpenAttack
 import nltk
+import datasets
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import numpy as np
 from tqdm import tqdm
@@ -12,6 +13,9 @@ def make_model():
             except LookupError:
                 nltk.download('vader_lexicon')
                 self.model = SentimentIntensityAnalyzer()
+        
+        def get_pred(self,input_):
+            return self.get_prob(input_).argmax(axis=1)
             
         def get_prob(self, input_):
             ret = []
@@ -24,33 +28,32 @@ def make_model():
 
 
 def main():
+    def dataset_mapping(x):
+        return{
+            "x":x["sentence"],
+            "y":1 if x["label"] > 0.5 else 0,
+        }
 
     print("Word-Level Attacks")
-    attacker = OpenAttack.attackers.PWWSAttacker()
     #attacker = OpenAttack.attackers.HotFlipAttacker()
+    attacker = OpenAttack.attackers.PWWSAttacker()
 
     print("Build model")
-    clsf = make_model()
+    victim = make_model() 
     
-    dataset = OpenAttack.DataManager.loadDataset("SST.sample")[15:20]
-
+    dataset = datasets.load_dataset("sst",split="train[:20]").map(function=dataset_mapping)
     print("Start attack")
-    options = {
-        "success_rate": True,
-        "fluency": False,
-        "mistake": False,
-        "semantic": False,
-        "levenstein": True,
-        "word_distance": False,
-        "modification_rate": True,
-        "running_time": True,
-
-        "invoke_limit": 500,
-        "average_invoke": True
-    }
-    attack_eval = OpenAttack.attack_evals.InvokeLimitedAttackEval(attacker, clsf, **options )
-    attack_eval.eval(dataset, visualize=True)
     
+    attack_eval = OpenAttack.AttackEval(attacker,victim,metrics=[
+        OpenAttack.metric.Fluency(),
+        OpenAttack.metric.GrammaticalErrors(),
+        OpenAttack.metric.SemanticSimilarity(),
+        OpenAttack.metric.EditDistance(),
+        OpenAttack.metric.ModificationRate()
+
+    ])
+    attack_eval.eval(dataset,visualize=True,progress_bar=True)
+      
     
 if __name__ == "__main__":
     main()
